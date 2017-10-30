47fcfc0a0bbfbfd67f1d228e182e666c27ef3efe
Boom244 <bolumsba@gmail.com>
2017-10-30 16:48:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require("fs");
-const token = fs.readFileSync("./token.txt", {"encoding": "utf-8"});
+
 function createResponse(content, reply)
 {
     bot.on('message', (message) => {
@@ -14,4 +14,4 @@ createResponse("!math", "The current math Homework is: 2 to 2.4")
 createResponse("!wonder", "Current Wonder Homework: Part 2: Via")
 createResponse("!commands", "!math: reminds math homework: !wonder: reminds Wonder Homework.")
 
-bot.login(token);
+bot.login(process.env.BOT_TOKEN);
